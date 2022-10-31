ac4a3e4de1a65a815d0c5d0ad3eba031119eb847
Evgeny Deriglazov <evgenyderiglazov@yandex.ru>
2022-10-31 20:42:58 +0300

    CREATE: models User, Subscriptions and register in Admin site

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import Subscriptions, User
+
+
+@admin.register(User)
+class CustomAdminUser(UserAdmin):
+    list_filter = ("email", "username")
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "email",
+                ),
+            },
+        ),
+    )
+
+
+@admin.register(Subscriptions)
+class SubscriptionsAdmin(admin.ModelAdmin):
+    ...
