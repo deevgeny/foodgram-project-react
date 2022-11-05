4f268a7e7fefe49c545a91109129f415644b2bc2
Evgeny Deriglazov <evgenyderiglazov@yandex.ru>
2022-11-05 12:57:40 +0300

    TEST: add tests for user app api

--- tests/fixtures/fixture_api.py
+++ tests/fixtures/fixture_api.py
@@ -1,8 +1,34 @@
 import pytest
+from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
+
+User = get_user_model()
 
 
 @pytest.fixture
 def api_client():
     client = APIClient()
     return client
+
+
+@pytest.fixture
+def api_user(db, api_client):
+    data = {
+        'email': 'user@fake.com',
+        'username': 'user',
+        'first_name': 'Vasya',
+        'last_name': 'Pupkin',
+        'password': 'VeryStrongPassword'
+    }
+    api_client.post('/api/users/', data=data)
+    return data['password'], data['email']
+
+
+@pytest.fixture
+def api_user_client(db, api_client, api_user):
+    password, email = api_user
+    response = api_client.post('/api/auth/token/login/',
+                               data={'password': password, 'email': email})
+    token = response.json()['auth_token']
+    api_client.credentials(HTTP_AUTHORIZATION=f'Token {token}')
+    return api_client
--- tests/test_09_api_registration.py
+++ tests/test_09_api_registration.py
@@ -2,10 +2,11 @@ from http import HTTPStatus
 
 import pytest
 
-reg_url = '/api/users/'
-login_url = '/api/auth/token/login/'
-logout_url = '/api/auth/token/logout/'
-change_password_url = '/api/users/set_password/'
+USERS_URL = '/api/users/'
+CURRENT_USER_URL = '/api/users/me/'
+LOGIN_URL = '/api/auth/token/login/'
+LOGOUT_URL = '/api/auth/token/logout/'
+CHANGE_PASSWORD_URL = '/api/users/set_password/'
 
 
 @pytest.mark.django_db
@@ -24,11 +25,11 @@ def test_user_registration(api_client):
         'first_name': 'Vasya',
         'last_name': 'Pupkin',
     }
-    response = api_client.post(reg_url, data=data)
+    response = api_client.post(USERS_URL, data=data)
     assert response.status_code != HTTPStatus.NOT_FOUND, (
-        f'url {reg_url} not found'
+        f'url {USERS_URL} not found'
     )
-    assert response.data == response_data, (
+    assert response.json() == response_data, (
         'Incorrect response data'
     )
     assert response.status_code == HTTPStatus.CREATED, (
@@ -38,31 +39,173 @@ def test_user_registration(api_client):
 
 @pytest.mark.django_db
 def test_user_registration_with_bad_data(api_client):
-    ...
-
-
-@pytest.mark.django_db
-def test_user_login(api_client):
     data = {
-        'email': 'vasya@fake.com',
         'username': 'vasek',
         'first_name': 'Vasya',
         'last_name': 'Pupkin',
         'password': 'VasyaStrong'
     }
-    api_client.post(reg_url, data=data)
-    response = api_client.post(
-        login_url, data={'password': data['password'], 'email': data['email']}
+    response_data = {'email': ['This field is required.']}
+    response = api_client.post(USERS_URL, data=data)
+    assert response.status_code != HTTPStatus.NOT_FOUND, (
+        f'url {USERS_URL} not found'
     )
-    assert 'auth_token' in response.data
-    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == response_data, (
+        'Incorrect response data'
+    )
+    assert response.status_code == HTTPStatus.BAD_REQUEST, (
+        f'Incorrect response status code {response.status_code}'
+    )
+
+
+@pytest.mark.django_db()
+def test_user_login(api_client, api_user):
+    password, email = api_user
+    response = api_client.post(
+        LOGIN_URL, data={'password': password, 'email': email}
+    )
+    assert response.status_code != HTTPStatus.NOT_FOUND, (
+        f'url {LOGIN_URL} not found'
+    )
+    assert 'auth_token' in response.json(), (
+        'Authorization token is missing in response'
+    )
+    assert response.status_code == HTTPStatus.CREATED, (
+        f'Incorrect response status code {response.status_code}'
+    )
+
+
+@pytest.mark.django_db()
+def test_user_login_with_bad_data(api_client):
+    response = api_client.post(
+        LOGIN_URL, data={'password': 'fakepass', 'email': 'some@fake.com'}
+    )
+    assert response.status_code != HTTPStatus.NOT_FOUND, (
+        f'url {LOGIN_URL} not found'
+    )
+    assert response.status_code == HTTPStatus.BAD_REQUEST, (
+        f'Incorrect response status code {response.status_code}'
+    )
 
 
 @pytest.mark.django_db
-def test_user_logout():
+def test_user_logout(api_user_client):
+    response = api_user_client.post(LOGOUT_URL)
+    assert response.status_code != HTTPStatus.NOT_FOUND, (
+        f'url {LOGOUT_URL} not found'
+    )
+    assert response.status_code == HTTPStatus.NO_CONTENT, (
+        f'Incorrect response status code {response.status_code}'
+    )
+
+
+@pytest.mark.django_db
+def test_user_change_password(api_user, api_user_client):
+    password, email = api_user
+    data = {'new_password': 'MyNewVeryStrongPassword',
+            'current_password': password}
+    response = api_user_client.post(CHANGE_PASSWORD_URL, data=data)
+    assert response.status_code != HTTPStatus.NOT_FOUND, (
+        f'url {CHANGE_PASSWORD_URL} not found'
+    )
+    assert response.status_code == HTTPStatus.NO_CONTENT, (
+        f'Incorrect response status code {response.status_code}'
+    )
+
+
+@pytest.mark.django_db
+def test_user_change_password_with_bad_data(api_user, api_user_client):
+    password, email = api_user
+    data = {'new_password': 'MyNewVeryStrongPassword',
+            'current_password': 'BadPassword'}
+    response = api_user_client.post(CHANGE_PASSWORD_URL, data=data)
+    assert response.status_code != HTTPStatus.NOT_FOUND, (
+        f'url {CHANGE_PASSWORD_URL} not found'
+    )
+    assert response.status_code == HTTPStatus.BAD_REQUEST, (
+        f'Incorrect response status code {response.status_code}'
+    )
+
+
+@pytest.mark.django_db
+def test_user_change_password_not_authorized(api_client):
+    data = {'new_password': 'MyNewVeryStrongPassword',
+            'current_password': 'BadPassword'}
+    response = api_client.post(CHANGE_PASSWORD_URL, data=data)
+    assert response.status_code != HTTPStatus.NOT_FOUND, (
+        f'url {CHANGE_PASSWORD_URL} not found'
+    )
+    assert response.status_code == HTTPStatus.UNAUTHORIZED, (
+        f'Incorrect response status code {response.status_code}'
+    )
+
+
+@pytest.mark.django_db
+def test_users_list_unauthorized(api_client):
+    response = api_client.get(USERS_URL)
+    assert response.status_code != HTTPStatus.NOT_FOUND, (
+        f'URL {USERS_URL} not found'
+    )
+    assert response.status_code == HTTPStatus.OK, (
+        f'Incorrect response status code {response.status_code}, '
+        'users list should be available for all'
+    )
+
+
+@pytest.mark.django_db
+def test_users_list_authorized(api_user_client):
+    response = api_user_client.get(USERS_URL)
+    assert response.status_code != HTTPStatus.NOT_FOUND, (
+        f'URL {USERS_URL} not found'
+    )
+    assert response.status_code == HTTPStatus.OK, (
+        f'Incorrect response status code {response.status_code}, '
+        'users list should be available for all'
+    )
+
+
+@pytest.mark.django_db
+def test_users_list_pagination(api_client):
+    # Add fixture with many users creation
     ...
 
 
 @pytest.mark.django_db
-def test_user_change_password():
+def test_user_info_by_id_unauthorized(api_user, api_client):
     ...
+
+
+@pytest.mark.django_db
+def test_user_info_by_id_authorized(api_user, api_client):
+    # Add fixture with many users creation
+    ...
+
+
+@pytest.mark.django_db
+def test_current_user_info_unauthorized(api_client):
+    response = api_client.get(CURRENT_USER_URL)
+    assert response.status_code != HTTPStatus.NOT_FOUND, (
+        f'URL {CURRENT_USER_URL} not found'
+    )
+    assert response.status_code == HTTPStatus.UNAUTHORIZED, (
+        f'Incorrect response status code {response.status_code}, '
+        'current user information should be available for '
+        'authorized users only'
+    )
+
+
+@pytest.mark.django_db
+def test_current_user_info_authorized(api_user, api_user_client):
+    password, email = api_user
+    response = api_user_client.get(CURRENT_USER_URL)
+    assert response.status_code != HTTPStatus.NOT_FOUND, (
+        f'URL {CURRENT_USER_URL} not found'
+    )
+    assert response.status_code == HTTPStatus.OK, (
+        f'Incorrect response status code {response.status_code}, '
+        'current user information should be available for '
+        'authorized users only'
+    )
+    assert response.json()['email'] == email, (
+        f'Incorrect current user information on {CURRENT_USER_URL}'
+    )
